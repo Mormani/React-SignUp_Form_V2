e93b742e8b76851c71e6582da915e6ea11e9f40a
Hugo Cesar <hugocesarcaldeira@gmail.com>
2021-12-13 16:09:30 -0300

    Personalizando formulario

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
-import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
+import {Typography} from "@material-ui/core";
 
 function FormCadastro({aoEnviar, validarCPF}) {
 
+    const [etapaAtual, setEtapaAtual] = useState(1);
+
+    function formularioAtual(etapa) {
+
+        switch(etapa) {
+            case 0: return  ( <DadosUsuario /> );
+            case 1: return  ( <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} /> );
+            case 2: return  ( <DadosEntrega /> );
+            default: return ( <Typography>Erro ao selecionar formulário</Typography> );
+        }
+    }
+
     return (
         <>
-            <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-            <DadosUsuario />
-            <DadosEntrega />
+            { formularioAtual(etapaAtual) }
         </>
     );
 }
