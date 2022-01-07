71f83c74fa3fe6d18def1146ac4278a318c23583
Hugo Cesar <hugocesarcaldeira@gmail.com>
2022-01-07 03:40:37 -0300

    Utilizando o useEffect e terminando o formulario

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {TextField, Button} from "@material-ui/core";
 
 function DadosEntrega({aoEnviar}) {
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,20 +1,37 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
+import {Step, StepLabel, Stepper, Typography} from "@material-ui/core";
 
 function FormCadastro({aoEnviar, validarCPF}) {
 
     const [etapaAtual, setEtapaAtual] = useState(0);
     function proxEtapa() { setEtapaAtual(etapaAtual + 1) };
 
+    const [dadosColetados, setDados] = useState({});
+    function coletarDados(dados) { setDados( {...dadosColetados, ...dados} ); proxEtapa(); };
+
+    useEffect(() => { if(etapaAtual === (forms.length - 1)) aoEnviar(dadosColetados); });
+
     const forms = [
-        <DadosUsuario  aoEnviar={proxEtapa} />,
-        <DadosPessoais aoEnviar={proxEtapa} validarCPF={validarCPF} />,
-        <DadosEntrega  aoEnviar={aoEnviar} />
+        <DadosUsuario  aoEnviar={coletarDados} />,
+        <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+        <DadosEntrega  aoEnviar={coletarDados} />,
+        <Typography variant="h5">Obrigado pelo Cadastro!</Typography>
     ];
 
-    return ( <>{ forms[etapaAtual] }</> );
+    return (
+        <>
+            <Stepper activeStep={etapaAtual}>
+                <Step><StepLabel>Login</StepLabel></Step>
+                <Step><StepLabel>Pessoal</StepLabel></Step>
+                <Step><StepLabel>Entrega</StepLabel></Step>
+                <Step><StepLabel>Finalização</StepLabel></Step>
+            </Stepper>
+            { forms[etapaAtual] }
+        </>
+    );
 }
 
 export default FormCadastro;
